14880f21b5f37a88749597ef2fa656519b45fdf5
Dominic Muttel <dominic.muttel@gmail.com>
2015-07-12 15:44:17 -0600

    had trouble adding to repository. git init->  git remote add origin ... -> git add . -> git commit -m ... -> git push -> git push origin master

---
+++ main.js
@@ -0,0 +1,25 @@
+for (i = 0; i <= 10; i++) {
+  console.log(i)
+}
+
+for (i = 0; i <= 1000; i = i + 100) {
+  console.log(i)
+}
+
+for (i = 1; i <= 128; i = i * 2) {
+  console.log(i)
+}
+
+for (i = 9; i >= 0; i--) {
+  console.log(i)
+}
+
+for (i = 1; i <= 4; i++) {
+  for (d = 3; d >= 1; d--) {
+    console.log(i); }
+}
+
+for (i = 0; i <= 4; i++) {
+  for (d = 0; d <= 4; d++) {
+  console.log(d); }
+}
